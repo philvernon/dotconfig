53b4701f7e14145cfab57ff2c4ceae7e87dc4817
philvernon <phil.vernon@synapt.ec>
2024-06-07 12:20:46 +0100

    delete nvim folder

--- nvim/lua/phil/lsp/lsp-mason.lua
+++
@@ -1,3 +0,0 @@
-require("mason-lspconfig").setup({
-	ensure_installed = { "volar", "tsserver", "lua_ls", "bashls", "gopls", "marksman" },
-})
--- nvim/lua/phil/markdown-preview.lua
+++
@@ -1,2 +0,0 @@
-vim.g.mkdp_browser = "/Applications/Google Chrome.app"
-vim.g.mkdp_theme = "dark"
