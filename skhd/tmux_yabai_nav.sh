378f6e0f7efe6ae1da7b7e54577091605f345b2a
philvernon <philip.s.vernon@gmail.com>
2026-08-15 16:42:26 +0100

    work on navigation interop over ssh

--- skhd/tmux_yabai_nav.sh
+++ skhd/tmux_yabai_nav.sh
@@ -2,12 +2,20 @@
 
 direction="$1"
 
-# case "$direction" in
-#     L) yabai -m window --focus west ;;
-#     R) yabai -m window --focus east ;;
-#     U) yabai -m window --focus north ;;
-#     D) yabai -m window --focus south ;;
-# esac
+echo "ITS WORKING"
+
+# If this pane is SSH, send Meta+direction through to the remote side.
+pane_cmd="$(tmux display-message -p '#{pane_current_command}')"
+
+if [[ "$pane_cmd" == "ssh" ]]; then
+    case "$direction" in
+				L) tmux send-keys M-h ;;
+				D) tmux send-keys M-j ;;
+				U) tmux send-keys M-k ;;
+				R) tmux send-keys M-l ;;
+    esac
+    exit
+fi
 
 # Fallback to yabai window focus
 case "$direction" in
@@ -16,5 +24,3 @@ case "$direction" in
     U) if [ $(tmux display-message -p '#{pane_at_top}') -ne 1 ]; then tmux select-pane -U; else yabai -m window --focus north || true; fi ;;
     D)  if [ $(tmux display-message -p '#{pane_at_bottom}') -ne 1 ]; then tmux select-pane -D; else yabai -m window --focus south || true; fi ;;
 esac
-#
-# Fallback to yabai window focus
